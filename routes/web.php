42ed78ac4bbd4b3b3a65502ca6483394d37bcc16
Rqq-al22 <rezkialya0909@gmail.com>
2026-07-08 11:13:43 +0800

    Secure production routing by removing debug endpoints

--- routes/web.php
+++ routes/web.php
@@ -28,23 +28,3 @@ Route::get('/absen.html', function () {
 Route::get('/admin.html', function () {
     return view('admin');
 });
-
-Route::get('/debug-env', function () {
-    return [
-        'APP_KEY_exists' => !empty(env('APP_KEY')),
-        'DATABASE_URL_exists' => !empty(env('DATABASE_URL')),
-        'APP_ENV' => env('APP_ENV'),
-        'APP_DEBUG' => env('APP_DEBUG'),
-        'all_env_keys' => array_keys($_ENV),
-        'all_server_keys' => array_keys($_SERVER),
-    ];
-});
-
-Route::get('/test-api', function () {
-    return [
-        'success' => true,
-        'members' => \App\Models\Member::orderBy('name', 'asc')->get()
-    ];
-});
-
-
